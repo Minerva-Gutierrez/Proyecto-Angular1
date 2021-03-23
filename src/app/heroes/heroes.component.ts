080e5596671a8dc23ae7ed5cc84e961df8275496
Minerva-Gutierrez <201721115@tese.edu.mx>
2021-03-23 14:04:10 -0600

    Se modifico a lista dinamica

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero} from '../hero';
+
+import {Hero} from '../hero';
+
+
+
+import {HEROES} from '../mock-heroes';
+
+
 
 @Component({
+
   selector: 'app-heroes',
+
   templateUrl: './heroes.component.html',
+
   styleUrls: ['./heroes.component.css']
+
 })
+
 export class HeroesComponent implements OnInit {
-  /*crear clase*/
-  hero: Hero={
-    id:1,
-    name:'hulk',
-    edad: 40,
-    superpoder:'super fuerza',
-    universo:'MARVEL',
-    identidad:'BRUCE'
-  };
+
+
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+
+
 
   constructor() { }
 
+
+
   ngOnInit(): void {
+
   }
+
+  onSelect(hero: Hero): void {
+
+    this.selectedHero = hero;
+
+  }
+
+
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,22 @@
+export const HEROES: Hero[]=[
+
+    {id:11,name:"superman",edad:30,superpoder:"extraterrestre",universo:"DC",identidad:"Sysntax.error"},
+
+    {id:12,name:"batman",edad:40,superpoder:"inteligencia",universo:"DC",identidad:"Sysntax.error"},
+    
+    {id:13,name:"hulk",edad:30,superpoder:"fuerza",universo:"MARVEL",identidad:"Sysntax.error"},
+    
+    {id:14,name:"iron man",edad:40,superpoder:"tecnologia",universo:"MARVEL",identidad:"Sysntax.error"},
+    
+    {id:13,name:"thor",edad:45,superpoder:"dios",universo:"MARVEL",identidad:"Sysntax.error"},
+    
+    {id:13,name:"spiderman",edad:21,superpoder:"trepar",universo:"MARVEL",identidad:"Sysntax.error"},
+    
+    {id:13,name:"black widow",edad:30,superpoder:"extraterrestre",universo:"MARVEL",identidad:"Sysntax.error"},
+    
+    {id:13,name:"flash",edad:25,superpoder:"veloz",universo:"DC",identidad:"Sysntax.error"},
+    
+    {id:13,name:"bruja escarlata",edad:35,superpoder:"extraterrestre",universo:"MARVEL",identidad:"Sysntax.error"},
+    
+    {id:13,name:"mujer maravilla",edad:30,superpoder:"amazona",universo:"DC",identidad:"Sysntax.error"}
+];
