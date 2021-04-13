46a9e744daa81ea552a556e47d9c77d7aae56de6
Minerva-Gutierrez <201721115@tese.edu.mx>
2021-04-13 17:59:37 -0500

    COMPONENT MESSAGE.SERVICE

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -10,15 +10,15 @@ export const HEROES: Hero[]=[
     
     {id:14,name:"IRON MAN",edad:40,superpoder:"tecnologia",universo:"MARVEL",identidad:"magnate"},
     
-    {id:13,name:"HOMBRE HORMIGA",edad:45,superpoder:"fuerza",universo:"MARVEL",identidad:"dios"},
+    {id:15,name:"ANT MAN",edad:45,superpoder:"fuerza",universo:"MARVEL",identidad:"dios"},
     
-    {id:13,name:"SPIDER MAN",edad:21,superpoder:"trepar",universo:"MARVEL",identidad:"estudiante"},
+    {id:16,name:"SPIDER MAN",edad:21,superpoder:"trepar",universo:"MARVEL",identidad:"estudiante"},
     
-    {id:13,name:"BLACK WINDOW",edad:30,superpoder:"extraterrestre",universo:"MARVEL",identidad:"cientifico"},
+    {id:17,name:"BLACK WIDOW",edad:30,superpoder:"extraterrestre",universo:"MARVEL",identidad:"cientifico"},
     
-    {id:13,name:"ACUAMAN",edad:37,superpoder:"fuerza",universo:"DC",identidad:"ingeniero"},
+    {id:18,name:"AQUAMAN",edad:37,superpoder:"fuerza",universo:"DC",identidad:"ingeniero"},
     
-    {id:13,name:"BRUJA ESCARLATA",edad:35,superpoder:"extraterrestre",universo:"MARVEL",identidad:"ama de casa"},
+    {id:19,name:"SCARLET WITCH",edad:35,superpoder:"extraterrestre",universo:"MARVEL",identidad:"ama de casa"},
     
-    {id:13,name:"MUJER MARAVILLA",edad:30,superpoder:"amazona",universo:"DC",identidad:"el amor de mi vida"}
+    {id:20,name:"WONDER WOMAN",edad:30,superpoder:"amazona",universo:"DC",identidad:"el amor de mi vida"}
 ];
